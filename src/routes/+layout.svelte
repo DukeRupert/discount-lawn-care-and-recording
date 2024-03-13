f4107915bef1026ce5cdeaeb9f4f88e4fa5efba2
Logan Williams <logan@fireflysoftware.dev>
2024-03-13 23:58:14 +0000

    Refactored Navbar component out of +page

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,14 @@
 <script>
 	import '../app.pcss';
+	import Navbar from '$lib/components/navbar/navbar.svelte';
 </script>
 
-<slot />
+<div class="bg-white">
+	<header class="absolute inset-x-0 top-0 z-50">
+		<Navbar />
+	</header>
+	<main>
+		<slot />
+	</main>
+	<footer></footer>
+</div>
